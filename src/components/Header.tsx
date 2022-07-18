7988a101469b58dba40fd7074b9741779c9fec2a
Leandro Fialho <leandro.fialho@prodoctor.net>
2022-07-18 17:37:31 -0300

    Add Btn deslogar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 import { List } from "phosphor-react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "./Logo";
 interface Menu {
   setOpenMenu: () => void,
 }
 
 export function Header(props: Menu) {
+  const navigate = useNavigate() // Redireciona o usuário para uma nova tela sem ter que clicar em um link # a
+
   return (
     <header className="w-full py-5 flex gap-4 justify-between px-6 place-content-center bg-gray-700 border-b border-gray-600 group">
       <Logo />
+      <button className="hidden xl:block border px-2 border-green-300 rounded" onClick={() => navigate('/')}>Deslogar</button>
       <button className="xl:hidden flex items-center gap-2 group" onClick={props.setOpenMenu}>
         <span className="text-sm">Aulas</span> 
         <List size={30} className="group-hover:text-blue-500"/> 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
@@ -32,15 +33,20 @@ interface Menu {
 
 export function SideBar(props: Menu) {
   // const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
-  const { data } = useGetLessonsQuery()
+  const { data } = useGetLessonsQuery();
+  const navigate = useNavigate() // Redireciona o usuário para uma nova tela sem ter que clicar em um link # a
 
   return (
-    <aside className={classNames(`w-[348px] bg-gray-700 p-6 border-l border-gray-600 xl:block` , {
-      'block absolute w-screen h-full' : props.menu,
-      'hidden' : !props.menu,
+    <aside className={classNames(`w-[348px] bg-gray-700 p-6 border-l border-gray-600 xl:block`, {
+      'block absolute w-screen h-full': props.menu,
+      'hidden': !props.menu,
     })}>
+      <div className="flex justify-end">
+        <button className="xl:hidden border px-2 mb-4 border-green-300 rounded" onClick={() => navigate('/')}>Deslogar</button>
+      </div>
+
       <h2 className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500">Cronograma das aulas</h2>
-      
+
       <div className="flex flex-col gap-8">
         {data?.lessons.map(lesson => {
           return (
